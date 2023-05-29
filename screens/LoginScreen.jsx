f6221c30d9c8291adc8abb5553548091a6c03fc6
Andrew Ketsko <monicman82@gmail.com>
2023-05-29 11:57:20 +0300

    2-3done

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -16,7 +16,7 @@ import ImageBG from "../PhotoBG.jpg";
 import { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { Entypo } from "@expo/vector-icons";
-
+import { styles } from "./scc";
 export default function LoginScreen() {
   const [passwordVisible, setPasswordVisible] = useState(true);
   const [focused, setFocused] = useState(null);
@@ -61,32 +61,35 @@ export default function LoginScreen() {
           <KeyboardAvoidingView
             behavior={Platform.OS == "ios" ? "padding" : "height"}
           >
-            <TextInput
-              style={
-                focused === "password"
-                  ? { ...styles.input, ...styles.focus }
-                  : { ...styles.input }
-              }
-              placeholder="Password"
-              name="password"
-              placeholderTextColor={"#BDBDBD"}
-              textContentType="password"
-              secureTextEntry={true}
-              value={password}
-              onChangeText={setPassword}
-              onFocus={setFocus}
-              onBlur={setBlur}
-              // <Entypo name="eye-with-line" size={24} color="black" />
-              // <Entypo name="eye" size={24} color="black" />
-              // right={
-              //   <TextInput.Icon
-              //     name={passwordVisible ? "eye" : "eye-with-line"}
-              //     size={24}
-              //     color="black"
-              //     onPress={() => setPasswordVisible(!passwordVisible)}
-              //   />
-              // }
-            />
+            <View style={{ position: "relative" }}>
+              <TextInput
+                style={
+                  focused === "password"
+                    ? { ...styles.input, ...styles.focus }
+                    : { ...styles.input }
+                }
+                placeholder="Password"
+                name="password"
+                placeholderTextColor={"#BDBDBD"}
+                textContentType="password"
+                secureTextEntry={passwordVisible}
+                value={password}
+                onChangeText={setPassword}
+                onFocus={setFocus}
+                onBlur={setBlur}
+              />
+              <Entypo
+                name={passwordVisible ? "eye" : "eye-with-line"}
+                size={24}
+                color="#212121"
+                style={{
+                  position: "absolute",
+                  right: 12,
+                  top: 12,
+                }}
+                onPress={() => setPasswordVisible((prev) => !prev)}
+              />
+            </View>
           </KeyboardAvoidingView>
           <TouchableOpacity style={styles.button} onPress={onPress}>
             <Text style={styles.buttonText}>Login</Text>
@@ -102,112 +105,3 @@ export default function LoginScreen() {
     </ImageBackground>
   );
 }
-
-const styles = StyleSheet.create({
-  focus: {
-    backgroundColor: "#Ffffff",
-    borderColor: "#FF6C00",
-  },
-  container: {
-    position: "relative",
-    marginTop: "auto",
-    // borderWidth: 1,
-    // borderStyle: "solid",
-    borderTopLeftRadius: 25,
-    borderTopRightRadius: 25,
-    paddingHorizontal: 16,
-    paddingTop: 32,
-    paddingBottom: 100,
-    backgroundColor: "white",
-    opacity: 1,
-  },
-  image: {
-    height: 120,
-    width: 120,
-    backgroundColor: "#F6F6F6",
-    position: "absolute",
-    top: -60,
-    left: "50%",
-    borderRadius: 16,
-    transform: [{ translateX: -50 }],
-    // overflow:'hidden',
-    // borderWidth: 1,
-    // borderStyle: "solid",
-  },
-  icon: {
-    position: "absolute",
-    right: -12,
-    bottom: 14,
-    zIndex: 100,
-  },
-  imageBG: {
-    flex: 1,
-    width: null,
-    height: null,
-    // opacity: 0.6
-  },
-  input: {
-    height: 50,
-    width: null,
-    backgroundColor: "#F6F6F6",
-    // border: 1 'solid' '#E8E8E8',
-    borderWidth: 1,
-    borderStyle: "solid",
-    borderRadius: 8,
-    borderColor: "#E8E8E8",
-    marginBottom: 16,
-    padding: 15,
-    fontFamily: "Roboto",
-    fontStyle: "normal",
-    fontSize: 16,
-    lineHeight: 19,
-    color: "#BDBDBD",
-  },
-  header: {
-    // width: 160,
-    height: 35,
-    // left: calc((50 % -160) / 2 + 0.5),
-    fontFamily: "Roboto",
-    fontStyle: "normal",
-    fontWeight: 500,
-    fontSize: 30,
-    lineHeight: 35,
-    textAlign: "center",
-    // letterSpacing: 0.01em,
-    color: "#212121",
-    marginBottom: 33,
-  },
-  button: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 100,
-    height: 50,
-    width: null,
-    backgroundColor: "#FF6C00",
-    marginTop: 27,
-  },
-  buttonText: {
-    fontFamily: "Roboto",
-    fontStyle: "normal",
-    fontWeight: 400,
-    fontSize: 16,
-    lineHeight: 19,
-    color: "#FFFFFF",
-  },
-  text: {
-    fontFamily: "Roboto",
-    fontStyle: "normal",
-    fontWeight: 400,
-    fontSize: 16,
-    lineHeight: 19,
-    textAlign: "center",
-    color: "#1B4371",
-    marginTop: 16,
-  },
-  bottomText: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-  },
-});
